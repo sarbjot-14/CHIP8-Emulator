75bae9f40937cda83f01680a9d885a2d5a1e7dab
Sarbjot Singh <sarbjot_14@hotmail.com>
2019-01-18 13:42:38 -0800

    finished translate button

--- Tool/tool.js
+++ Tool/tool.js
@@ -1,6 +1,6 @@
 console.log("this is the tool");
 
-var spriteArray = [0,1,0]
+var spriteArray = []
 
 function invertColor(x){
   if(x.style.backgroundColor=="black"){
@@ -17,4 +17,31 @@ function resetPixels(){
   for (var i = 0; i < elements.length; i++) {
     elements[i].style.backgroundColor="black";
   }
+}
+
+function translateSprite(){
+  elements = document.getElementsByClassName("pixel");
+  for (var i = 0; i < elements.length; i++) {
+    if(elements[i].style.backgroundColor=="black"){
+
+      spriteArray.push(0);
+    }
+    else{
+      spriteArray.push(1);
+    }
+
+  }
+
+  var outputBox= document.getElementById("outputBox");
+  var outputString;
+
+  for(var i=0; i<15 ; i++){
+    for(var j= 0; j<8; j++){
+      outputBox.innerHTML+= spriteArray[j+8*i];
+    }
+    outputBox.innerHTML+= ",\n";
+  }
+
+  console.log("the array is:"+ spriteArray);
+
 }
