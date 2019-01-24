005a31839822cceec55188cf74ca3df56d74cb87
Adam Nizol <adam@nizol.ca>
2019-01-23 21:43:51 -0800

    add parser outline

---
+++ js/parser.js
@@ -0,0 +1,20 @@
+//Feel free to rename str to something better
+
+class chipParser{
+
+  //Pre-Conditions:
+  //  -str is a string representing a mnemonic chip file with new line characters separating each command
+  // Note: there may be empty lines or comments that should be filtered out.
+  //Post-Conditions:
+  //  -returns parsed result as a hex string
+  //  -throws error on invalid commands
+  parse(str){
+    //...
+
+  }
+
+
+  //Helper functions:
+
+
+}
