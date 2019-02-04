9d6587f512c43684c9567da144b0129ae9751bc1
Sarbjot Singh <sarbjot_14@hotmail.com>
2019-02-04 14:09:12 -0800

    fixed tool, Now keeps blank lines within sprite

--- Tool/tool.js
+++ Tool/tool.js
@@ -22,6 +22,8 @@ function resetPixels(){
 function translateSprite(){
   let outputBox= document.getElementById("outputBox");
   let outputString;
+  let startRow = 0; //figure out what row the sprite starts and ends
+  let lastRow = 0;
   outputBox.innerHTML = "";
 
   spriteArray = [];
@@ -35,19 +37,29 @@ function translateSprite(){
     }
   }
 
+  for(let i=0; i<15 ; i++){ //find startRow and endRow
+    let binRow = "";
+    for(let j= 0; j<8; j++){
+      if(startRow==0 && spriteArray[j+8*i] == 1 ){ //set the startRow
+        startRow = i;
+      }
+      if(i > lastRow && spriteArray[j+8*i] == 1 ){ //update endRow
+        lastRow = i;
+      }
+
+      binRow += spriteArray[j+8*i];
+    }
+  }
+   //print lines (output translations)
   for(let i=0; i<15 ; i++){
     let binRow = "";
     for(let j= 0; j<8; j++){
       binRow += spriteArray[j+8*i];
     }
-    if(binRow!="00000000"){
+    if(i >= startRow && (i<= lastRow) ){
       outputBox.innerHTML+= "Bin: "+ binRow + "  |  Hex: " + byteToHex(binRow) + "\n";
     }
-  
   }
-
-  console.log("the array is:"+ spriteArray);
-
 }
 
 function byteToHex(binStr){
