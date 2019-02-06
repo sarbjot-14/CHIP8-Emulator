bdc4729bb7610e729bd62cd2009c98e154efa0e1
shisata <46393255+shisata@users.noreply.github.com>
2019-02-06 14:01:30 -0800

    Testing console.log outputing instruction name

--- js/emulator.js
+++ js/emulator.js
@@ -312,6 +312,7 @@ class emulator{
     let nnn = ins.substring(1,4);
     //console.log(ins) //enable this line to get opcode readouts
     switch(ins[0]){
+      console.log("Running instruction " + ins);
       case "0":
         switch(ins.substring(1,4)){
           case "0E0":// 00E0 - CLS - Clear the display
