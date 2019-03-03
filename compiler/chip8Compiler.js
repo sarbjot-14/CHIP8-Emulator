fba05b4045838b57d3832c551c72c40ceeebd940
Sarbjot Singh <sarbjot_14@hotmail.com>
2019-03-02 16:13:34 -0800

    added some assembly to opcode conversions

--- compiler/chip8Compiler.js
+++ compiler/chip8Compiler.js
@@ -7,16 +7,14 @@ class chip8Compiler{
   compileMneonicToOpcodes(code){
     let result = this.removeComments(code);
     result = this.removeEmptyLines(result);
-    let assemblyArray = this.splitAssembly(result);
-
-
+    let assemblyArray = result.split("\n");
     //convert assembly to opcode one line at a time
-    for(let x=0 ; x< assemblyArray.size ; x++){
+    //console.log("calling loop " + assemblyArray.size);
+    //console.log("size = " + assemblyArray.length);
 
-      command = this.replaceAssemblyWithOpcode(command);
-      assemblyArray[x] = command;
-      console.log(command);
-    }
+    assemblyArray = this.assemblyToOpcode(assemblyArray);
+    //assemblyArray = this.replaceAssemblyWithOpcode(assemblyArray);
+
     //final opcodes is the good version of the opcodes
     let finalOpcodes = "";
     assemblyArray.forEach(function(command) {
@@ -32,73 +30,131 @@ class chip8Compiler{
     console.log(result);
     return result;
   }
-  splitAssembly(code){
-    let assemblyArray = code.split("\n");
-    return assemblyArray;
 
-  }
   removeEmptyLines(code){
     let result = code.replace(/\t+/g,"");
     result = result.replace(/^\s*\n/gm, "");
     return result;
   }
+  assemblyToOpcode(assemblyArray){
+    console.log("wtf");
+    console.log(assemblyArray);
 
-  replaceAssemblyWithOpcodes(code){
-    let result = code;
-    //00E0 - CLS
-    result = code.replace(/CLS/, "00E0");
+    for(var x=0 ; x< assemblyArray.length ; x++){
+      let opcode= "";
+      let code = assemblyArray[x];
+      //00E0 - CLS
+      let r = /CLS/;
+      if(r.test(code)){
+        opcode = code.replace(/CLS/, "00E0");
+      }
+
+      //00EE - RET
+      //0nnn - SYS addr
+      //1nnn - JP addr
+      //2nnn - CALL addr
+      //3xkk - SE Vx, byte
+      //4xkk - SNE Vx, byte
+      //5xy0 - SE Vx, Vy
+      //6xkk - LD Vx, byte
+      r = /\b^LD\sV[0-9A-F],\s[0-9A-F]{1,2}\b/i;
+      if(r.test(code)){
+        let register =code.substring(4,5);
+        let byte = code.match(/\d+$/)[0];
+        if(byte.length ==1){
+          byte = "0" + byte;
+        }
+        //console.log(" register is " + code + "and "+ register +" " + byte);
+        opcode = code.replace(/\b^LD\sV[0-9A-F],\s[0-9A-F]{1,2}\b/i, "6"+ register + byte);
+      }
+
+      //7xkk - ADD Vx, byte
+
+      /////////GROUP FROM 8xy0 TO 8xy5///////////
+      //8xy0 - LD Vx, Vy
+      //8xy1 - OR Vx, Vy
+      //8xy2 - AND Vx, Vy
+      //8xy3 - XOR Vx, Vy
+      //8xy4 - ADD Vx, Vy
+      //8xy5 - SUB Vx, Vy
+      r = /\b(LD|OR|AND|XOR|ADD|SUB)\sV[1-9a-f],\sV[1-9a-f]\b/i;
+      if(r.test(code)){
+        let register1 =code.substring(5,6);
+        let regester2 = code.substring(9,10);
+        let number = 0;
+        if(code.substring(0,2)=="LD"){
+          number = 0;
+        }
+        if(code.substring(0,2)=="OR"){
+          number = 1;
+        }
+        if(code.substring(0,3)=="AND"){
+          number = 2;
+        }
+        if(code.substring(0,3)=="XOR"){
+          number = 3;
+        }
+        if(code.substring(0,3)=="ADD"){
+          number = 4;
+        }
+        if(code.substring(0,3)=="SUB"){
+          number = 5;
+        }
+        //console.log(" register is " + code + "and "+ register +" " + byte);
+        opcode = code.replace(r, "8"+ register1+ regester2 +number);
+      }
+      //8xy6 - SHR Vx {, Vy}
+      //8xy7 - SUBN Vx, Vy
+      //8xyE - SHL Vx {, Vy}
+      //9xy0 - SNE Vx, Vy
 
 
-    //00EE - RET
-    //0nnn - SYS addr
-    //1nnn - JP addr
-    //2nnn - CALL addr
-    //3xkk - SE Vx, byte
-    //4xkk - SNE Vx, byte
-    //5xy0 - SE Vx, Vy
-    //6xkk - LD Vx, byte
-    result = result.replace(/LD\sV[0-9A-F],\s\b(1?[0-9]{1,2}|2[0-4][0-9]|25[0-5])\b/g, "6xkk");
-    //7xkk - ADD Vx, byte
-    //8xy0 - LD Vx, Vy
-    //8xy1 - OR Vx, Vy
-    //8xy2 - AND Vx, Vy
-    //8xy3 - XOR Vx, Vy
-    //8xy4 - ADD Vx, Vy
-    //8xy5 - SUB Vx, Vy
-    //8xy6 - SHR Vx {, Vy}
-    //8xy7 - SUBN Vx, Vy
-    //8xyE - SHL Vx {, Vy}
-    //9xy0 - SNE Vx, Vy
-    /*
-    Annn - LD I, addr
-    Bnnn - JP V0, addr
-    Cxkk - RND Vx, byte
-    Dxyn - DRW Vx, Vy, nibble
-    Ex9E - SKP Vx
-    ExA1 - SKNP Vx
-    Fx07 - LD Vx, DT
-    Fx0A - LD Vx, K
-    Fx15 - LD DT, Vx
-    Fx18 - LD ST, Vx
-    Fx1E - ADD I, Vx
-    Fx29 - LD F, Vx
-    Fx33 - LD B, Vx
-    Fx55 - LD [I], Vx
-    Fx65 - LD Vx, [I]
-  3.2 - Super Chip-48 Instructions
-    00Cn - SCD nibble
-    00FB - SCR
-    00FC - SCL
-    00FD - EXIT
-    00FE - LOW
-    00FF - HIGH
-    Dxy0 - DRW Vx, Vy, 0
-    Fx30 - LD HF, Vx
-    Fx75 - LD R, Vx
-    Fx85 - LD Vx, R
-      */
-      return result;
+      //Annn - LD I, addr
+      //Bnnn - JP V0, addr
+      //Cxkk - RND Vx, byte
+      //Dxyn - DRW Vx, Vy, nibble
+      r = /\b^DRW\sV[0-9a-f],\sV[0-9a-f],\s[0-9a-f]\b/i;
+      if(r.test(code)){
+        let register1 =code.substring(5,6);
+        let regester2 = code.substring(9,10);
+        let nibble = code.match(/\d+$/)[0];
+        //console.log(" register is " + code + "and "+ register +" " + byte);
+        opcode = code.replace(r, "D"+ register1+ regester2 + nibble);
+      }
+      //Ex9E - SKP Vx
+      //ExA1 - SKNP Vx
+      //Fx07 - LD Vx, DT
+      //Fx0A - LD Vx, K
+      //Fx15 - LD DT, Vx
+      //Fx18 - LD ST, Vx
+      //Fx1E - ADD I, Vx
+      //Fx29 - LD F, Vx
+      //Fx33 - LD B, Vx
+      //Fx55 - LD [I], Vx
+      //Fx65 - LD Vx, [I]
+      /*
+    3.2 - Super Chip-48 Instructions
+      00Cn - SCD nibble
+      00FB - SCR
+      00FC - SCL
+      00FD - EXIT
+      00FE - LOW
+      00FF - HIGH
+      Dxy0 - DRW Vx, Vy, 0
+      Fx30 - LD HF, Vx
+      Fx75 - LD R, Vx
+      Fx85 - LD Vx, R
+        */
+      if(opcode == ""){
+        console.log("ERROR IN LINE: " +code);
+        assemblyArray[x] = code; //deltet this once all codes are done
+      }
+      else{
+        assemblyArray[x] = opcode;
+      }
+
+    }
+
+    return assemblyArray;
   }
-
-
 }
